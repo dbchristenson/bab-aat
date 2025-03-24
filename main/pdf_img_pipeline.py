6d230681e40084e8ee629a4a7a9ec72a0b65eb32
dbchristenson <dbeauchristenson@gmail.com>
2025-03-24 10:36:31 +0800

    first iteration of populating database with document and page information inside of pdf to img pipeline before debugging procedures

--- main/pdf_img_pipeline.py
+++ main/pdf_img_pipeline.py
@@ -1,19 +1,46 @@
 import argparse
 import glob
+import logging
 import os
 
 import pypdfium2 as pdfium
-from utils.page_to_img import create_img_and_pad_divisible_by_32
+from models import Document, Page
 from utils.configs import with_config
+from utils.page_to_img import create_img_and_pad_divisible_by_32
 
 BASE_DIR = "/Users/dbchristenson/Desktop/python/bab-aat/bab-aat"
 DATA_DIR = os.path.join(BASE_DIR, "data")
 OUTPUT_DIR = os.path.join(DATA_DIR, "output")
 
-def check_already_converted(file_path: str, output_dir: str) -> bool:
-    pattern = os.path.join(output_dir, f"{file_path[:-2]}*.png")
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),  # Log to a file
+        logging.StreamHandler(),  # Log to the console
+    ],
+)
 
-    return glob.glob(pattern)
+
+def check_already_converted(path: str, output_dir: str) -> tuple[bool, str]:
+    file_name = path.split("/")[-1]
+    pattern = os.path.join(output_dir, f"{file_name[:-2]}*.png")
+
+    converted = glob.glob(pattern)
+    if converted:
+        logging.info(f"{path} already converted, skipping...")
+
+    return converted, file_name
+
+
+def save_document(file_name: str, file_path: str) -> None:
+    """
+    Save a document to the database.
+    """
+    new_document = Document(name=file_name, file_path=file_path)
+    new_document.save()
+    logging.info(f"Document {file_name} added to the database.")
 
 
 def get_pdf_paths(
@@ -21,11 +48,12 @@ def get_pdf_paths(
 ) -> list[str]:
     """
     Get the paths of all PDF files in the specified directory. When working
-    locally on my mac, I already defined a base and data directory. When
+    locally, I already defined a base and data directory. When
     working elsewhere, define the absolute path to the directory containing
     the PDF files.
     """
 
+    # Setup and verify the PDF data directory
     if absolute_path:
         pdf_data_dir = dir
     else:
@@ -34,28 +62,41 @@ def get_pdf_paths(
     pdf_paths = [f for f in os.listdir(pdf_data_dir) if f.endswith(".pdf")]
     assert len(pdf_paths) > 0, f"No PDF files found in {pdf_data_dir}"
 
+    # Collect the full paths of the PDF files
     full_pdf_paths = []
 
     for path in pdf_paths:
         # Check if the PDF defined by the path already has been converted
-        file_name = path.split("/")[-1]
-        converted = check_already_converted(file_name, output_dir)
-
+        converted, file_name = check_already_converted(path, output_dir)
         if converted:
-            print(f"{file_name} already converted, skipping...")
             continue
 
-        # Join the directory and the path to get the absolute path
         new_path = os.path.join(pdf_data_dir, path)
 
         # Check if the path is a file
         if os.path.isfile(new_path):
             full_pdf_paths.append(new_path)
         else:
-            print(f"{new_path} is not a file")
+            logging.warning(f"{new_path} is not a file")
             continue
 
+        # Add the document to the database
+        save_document(file_name, new_path)
+
     return full_pdf_paths
+
+
+def save_img(parent_doc: Document, page_num: int, img_path: str):
+    """
+    Save the image to the output directory and add the page to the database.
+    """
+
+    # Save the image
+    new_page = Page(
+        document=parent_doc, page_number=page_num, img_path=img_path
+    )
+    new_page.save()
+    logging.info(f"Page {page_num} of {parent_doc.name} saved.")
 
 
 def convert_pdfs(pdf_paths: list, output_dir: str, scale: int = 4):
@@ -65,13 +106,6 @@ def convert_pdfs(pdf_paths: list, output_dir: str, scale: int = 4):
         base_name = file_name.split(".")[0]
         pdf = pdfium.PdfDocument(path)
 
-        """
-        print(f"Converting {file_name} to image...")
-        print(path)
-        print(type(pdf))
-        print([p for p in pdf][0].render(scale=scale))
-        """
-
         for i, page in enumerate(pdf):
             # Convert the page to an image
             img = create_img_and_pad_divisible_by_32(page, scale=scale)
@@ -80,10 +114,16 @@ def convert_pdfs(pdf_paths: list, output_dir: str, scale: int = 4):
             new_name = f"{base_name}_{i}.png"
             new_path = os.path.join(output_dir, new_name)
             img.save(new_path)
+            logging.info(f"Page {i} of {file_name} saved as {new_name}.")
+
+            # Save the image to the database
+            parent_doc = Document.get(Document.name == file_name)
+            save_img(parent_doc, i, new_path)
 
         pdf.close()
 
     return
+
 
 @with_config
 def main_pipeline(config=None):
@@ -96,7 +136,7 @@ def main_pipeline(config=None):
     pdf_paths = get_pdf_paths(pdf_dir, output_dir, absolute_path=absolute_path)
     convert_pdfs(pdf_paths, output_dir, scale=scale)
 
-    print("Done converting PDFs to images.")
+    logging.info("PDFs converted to images.")
 
     return
 
